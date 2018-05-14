5c299e89a5184469c42bf04cb3b89136c3f66390
Thirawuth <champ@odds.team>
2018-05-14 12:24:45 +0700

    mock heroes

---
+++ learnHero/src/app/mock-heroes.ts
@@ -0,0 +1,8 @@
+import { Hero } from './Hero'
+
+export const HEROES: Hero[] = [
+    {id: 11, name: 'Hero Eleven'},
+    {id: 12, name: 'Hero Twelve'},
+    {id: 13, name: 'Hero Thirteen'},
+    {id: 14, name: 'Hero Forteen'}
+]
